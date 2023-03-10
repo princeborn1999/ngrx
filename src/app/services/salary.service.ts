6519119464c635fabb98dc8dad5756caf303d2dd
AaronPrince <princeborn1999@gmail.com>
2023-03-10 17:38:24 +0800

    [+] 新增ngrx相關ts檔案

---
+++ src/app/services/salary.service.ts
@@ -0,0 +1,11 @@
+import { Store } from '@ngrx/store';
+import { Injectable } from '@angular/core';
+import { AppState } from '../store/app.state';
+@Injectable({
+  providedIn: 'root'
+})
+export class SalaryService {
+
+  constructor() { }
+
+}
