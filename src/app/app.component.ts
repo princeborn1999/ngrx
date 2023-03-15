abe808033210e8d4b22d4f6ca5b8619b346dfa01
tinaho <tinaho@fstop.com.tw>
2023-03-15 17:17:40 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+
+interface Action {
+  type: string;
+  payload?: any
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,3 +26,16 @@ export class AppComponent implements OnInit {
 
   }
 }
+
+class Dispatcher extends Subject<Action> {
+  dispatch(action: Action) {
+    this.next(action);
+  }
+}
+
+const dispatcher = new Dispatcher();
+const sub1 = dispatcher.subscribe(res => console.log('sub1',res))
+const sub2 = dispatcher.subscribe(res => console.log('sub2',res))
+
+dispatcher.dispatch({type: 'Action1'})
+dispatcher.dispatch({type: 'Action2'})
