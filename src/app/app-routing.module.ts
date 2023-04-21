51e8d4aaf62709ac47f9dd2a47b52a464657d325
AaronPrince <princeborn1999@gmail.com>
2023-04-21 15:13:08 +0800

    新增: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './component/home/home.component';
+import { CartComponent } from './component/cart/cart.component';
+const routes: Routes = [{path: 'index', component: HomeComponent},
+                        {path: 'cart', component: CartComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
