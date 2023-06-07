a00239ff07dfefe8d852b42d0fde4e6683784011
AaronPrince <princeborn1999@gmail.com>
2023-06-07 11:27:40 +0800

    remove karma jasmine

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,8 @@
 module.exports = {
   preset: 'jest-preset-angular',
+  testMatch: ['**/*.spec.ts'],
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-    '<rootDir>/src/test.ts',
-  ],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$',
-      astTransformers: {
-        before: [
-          'jest-preset-angular/build/InlineFilesTransformer',
-          'jest-preset-angular/build/StripStylesTransformer',
-        ],
-      },
-    },
-  },
+  collectCoverage: true,
+  coverageReporters: ['html', 'lcovonly', 'text-summary']
 };
+
--- src/app/component/coupon/coupon.component.spec.ts
+++ src/app/component/coupon/coupon.component.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CouponComponent } from './coupon.component';
-
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 describe('CouponComponent', () => {
   let component: CouponComponent;
   let fixture: ComponentFixture<CouponComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CouponComponent ]
-    })
-    .compileComponents();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CouponComponent
+      ],
+    }).compileComponents();
+  }));
 
-    fixture = TestBed.createComponent(CouponComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  it('should create',  async(() => {
     expect(component).toBeTruthy();
+  }));
+
+  it('should initialize coupon properties correctly', () => {
+    expect(component.couponId).toBe('1');
+    expect(component.couponType).toBe('Discount');
+    expect(component.couponName).toBe('$1,000免運券');
+    expect(component.couponDescription).toBe('低消$1,000');
+    expect(component.limit).toBe(1);
+    expect(component.discount).toBe(10);
+    expect(component.priceOff).toBeUndefined();
+    expect(component.deliveryFree).toBe(true);
   });
 });
